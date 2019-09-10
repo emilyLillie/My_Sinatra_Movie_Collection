282b1a520b4c918e4fb3b74478f23dc10b2fea57
Emily Lillie <emilylillie3@gmail.com>
2019-09-10 17:24:01 +0000

    gems wouldnt install, trouble shooting to fix

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,19 @@
+# ENV['SINATRA_ENV'] ||= "development"
+
+# require 'bundler/setup'
+# Bundler.require(:default, ENV['SINATRA_ENV'])
+
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
+
+# require_all 'app'
+
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
-
-require_all 'app'
+require './app'
+require_all 'models'
