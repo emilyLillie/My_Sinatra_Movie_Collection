8c329ff66eae268dfb8349b9e7c76e4fae5a7e7e
Emily Lillie <emilylillie3@gmail.com>
2019-09-13 02:00:47 +0000

    added genre and movie model and migration

---
+++ db/migrate/20190913014855_create_movies.rb
@@ -0,0 +1,9 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.integer :user_id
+      t.integer :genre_id
+      t.text :title 
+    end 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190913015434) do
+
+  create_table "genre", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "genre_id"
+    t.text    "title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
 
 end
