8409657d89793d8bb9750f7fb8e8c831083b8a6c
Emily Lillie <emilylillie3@gmail.com>
2019-09-15 23:27:04 +0000

    added edit get post and erb

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -23,12 +23,25 @@ class MovieController < ApplicationController
   
   post '/movies/new' do 
     @movie = Movie.create(:title => params[:title], :genre => params[:genre])
+    @movie.save
     redirect to "/movies/#{Movie.last.id}"
   end
   
   get '/movies/:id' do 
     @movie = Movie.find_by_id(params[:id])
     erb :movie_info
+  end 
+  
+  get '/movies/:id/edit' do 
+    @movie = Movie.find_by_id(params[:id])
+    erb :'/movies/edit'
+  end 
+  
+  patch '/movies/:id/edit' do 
+    @movie = Movie.find_by_id(params[:id])
+    @movie.title = params[:title]
+    @movie.genre = params[:genre]
+    @movie.save
   end 
   
 end 
