f1a139727c2e811d1367ab7f87f18af2cb5db9f5
Emily Lillie <emilylillie3@gmail.com>
2019-09-25 00:37:21 +0000

    everything broke for no reason

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -9,7 +9,7 @@ class MovieController < ApplicationController
      @user = User.find(session[:user_id])
     # do i need @user?
      @movies = Movies.all.select {|movie| movie.user_id == session[:user_id]}
-     erb :'/movies/index' 
+     erb :'/movies/home' 
     else 
       redirect to "/login"
     end
--- app/models/movies.rb
+++ app/models/movies.rb
@@ -1,5 +1,4 @@
 class Movies < ActiveRecord::Base 
-  # belongs_to :genre
   belongs_to :user 
   
 end 
